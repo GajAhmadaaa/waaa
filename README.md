f15b82977e07ef5410581e98fcbd6c0813c2ceb5
GajAhmadaaa <64576446+GajAhmadaaa@users.noreply.github.com>
2021-02-25 03:38:03 +0700

    aaa
    aaa

---
+++ README.md
@@ -0,0 +1,33 @@
+# aqua-wa-automate
+Aqua Bot
+
+note: use  jikan="https://api.jikan.moe/v3/" in feature.js in debug environment
+Status :working
+
+Todo:
+-
+- Update This Readme
+- Reset Structure
+- Gif To Sticker
+- Plugin System
+- Express Control Panel
+- Moderation System(Non priortized)
+
+---
+
+Use:
+-
+- npm install
+- node server.js
+(After this scan qr in console )
+  
+
+  
+Issues:
+  -
+  No Issues
+  
+ ## Prerequisites
+ - Node.js
+
+
---
+++ index.js
@@ -0,0 +1,131 @@
+
+
+const feature = require('./features.js');
+const wa = require('@open-wa/wa-automate');
+const { create, decryptMedia, ev } = wa;
+const request = require('request');
+const fs = require('fs');
+const jikan="https://api.jikan.moe/v3/";
+//const jikan="http://192.168.0.108:8000/v3/";
+/*const launchConfig = {
+    useChrome: true,
+    autoRefresh:true,
+    disableSpins: true,
+    blockCrashlogs: true,
+    headless: true,
+   // cacheEnabled:false,
+    executablePath: "/usr/bin/chromium-browser",
+  //  customUserAgent:"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+  //   cacheEnabled: false,
+  //   chromiumArgs: [
+  //       '--no-sandbox',
+  //       '--disable-setuid-sandbox'
+//    ]
+};
+wa.create(launchConfig).then(client => start(client));
+*/
+/**
+ * Initialize client
+ * @param {import("@open-wa/wa-automate").Client} client
+ */
+async function start(client) {
+  queue.start();
+  const unreadMessages = await client.getAllUnreadMessages();
+  unreadMessages.forEach(processMessage);
+  client.onMessage(processMessage);
+}
+
+ev.on("qr.**", async (qrcode) => {
+  const imageBuffer = Buffer.from(
+    qrcode.replace("data:image/png;base64,", ""),
+    "base64"
+  );
+  fs.writeFileSync("./public/qr_code.png", imageBuffer);
+});
+
+create({
+  qrTimeout: 0,
+  cacheEnabled: false,
+}).then((client) => start(client));
+
+server.use(express.static("public"));
+server.listen(PORT, () =>
+  console.log(`> Listining on http://localhost:${PORT}`)
+);
+
+process.on("exit", () => {
+  if (fs.existsSync("./session.data.json")) {
+    fs.unlinkSync("./session.data.json");
+  }
+function start(client) {
+    client.onMessage(message => {
+       // if(message.isGroupMsg ){
+            if (message.body.startsWith("/anime")) {
+            feature.animeSearch(client,message,message.body.slice(6))
+            }else if (message.body.startsWith("/manga")) {
+                feature.mangaSearch(client,message,message.body.slice(6))
+            }else if (message.body.startsWith("/sauce")) {
+                feature.souceAdapter(client,message)
+            }else if (message.body.startsWith("/poll")) {
+                feature.getpoll(client,message)
+            } else if (message.body.startsWith("/vote")) {
+                feature.voteadapter(client,message)
+            } else if (message.body.startsWith("/resetpoll")) {
+                 feature.adminpollreset(client,message,message.body.slice(10))
+            } else if (message.body.startsWith("/add")) {
+                 feature.addcandidate(client,message,message.body.slice(4))
+            } else if (message.body.toLowerCase().startsWith("/art")){
+                feature.redditContent(client,message,"awwnime");
+            } else if (message.body.toLowerCase().startsWith("/meme")){
+                feature.redditContent(client,message,"");
+            } else if (message.body.toLowerCase().startsWith("/wall")){
+                feature.redditContent(client,message,"Animewallpaper");
+            } else if (message.body.toLowerCase().startsWith("/help")){
+                feature.helpContent(client,message,);
+            } else if (message.body.toLowerCase().startsWith("/sticker") || message.body.toLowerCase().startsWith("/stiker")){
+                 feature.sticker(client,message,);
+            }  else if (message.body.toLowerCase().includes("chat.whatsapp.com/")){
+                client.reply(message.from,"*WARNING*\n Invite links not Allowed",message.id);
+            } else if (message.body.toLowerCase().startsWith("/rules")){
+                //rulesContent(client,message,);
+                // saveJsonFile("test.json",{name:"shubham"})
+                // console.log(readJsonFile("test.json").name)
+                client.getChatById(message.chatId).then(value => {
+                    const { desc } = value.groupMetadata
+                    client.reply(message.from, '*' + message.chat.name + '*\n🌠️\n✨️ Description:\n ' + `${desc}`,message.id,true)
+                })
+
+               //client.sendStickerfromUrl(message.from,"https://svg2png1.glitch.me/?u="+message.sender.pushname+"&rank=12&level=307");
+                //client.reply(message.chatId,message.sender.formattedName+" | "+message.sender.name+" | "+message.sender.shortName+" | "+message.sender.pushname,message.id);//Shubham Badgujar Jio | Shubham Badgujar Jio | Shubham | Magician Master
+            }
+
+      //  }
+
+
+
+        // if (message.body === 'Hi') {
+        //    // client.sendStickerfromUrl(message.from,"https://raw.githubusercontent.com/open-wa/wa-automate-nodejs/master/release.png")
+        //
+        //     client.sendImageAsSticker(message.from,)
+        //     //client.sendText(message.from, '👋 Hello!');
+        // }
+    });
+}
+
+
+
+// client.getGroupInviteLink(message.chatId).then(value => {
+//     console.log(value)
+// }).catch(reason => {
+//     console.log(reason)
+// })
+
+// client.inviteInfo("https://chat.whatsapp.com/LJYGah65GKtBChoIttot3W").then(value => {
+//     console.log(value)
+// })
+
+
+// client.getGroupAdmins(message.chatId).then(value => {
+//     console.log(value+message.author)
+//     console.log(value.toString().includes(message.author))
+// });
